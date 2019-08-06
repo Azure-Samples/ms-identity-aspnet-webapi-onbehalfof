27c9b9fd4240273a003b7b4d04dfd0e90a70fbe1
Tiago Brenck <v-tibre@microsoft.com>
2019-08-06 12:27:22 -0700

    updating Cleanup.ps1 to remove Service Principles

--- AppCreationScripts/Cleanup.ps1
+++ AppCreationScripts/Cleanup.ps1
@@ -5,7 +5,7 @@ param(
     [string] $tenantId
 )
 
-if ((Get-Module -ListAvailable -Name "AzureAD") -eq $null) { 
+if ($null -eq (Get-Module -ListAvailable -Name "AzureAD")) { 
     Install-Module "AzureAD" -Scope CurrentUser 
 } 
 Import-Module AzureAD
@@ -44,35 +44,59 @@ This function removes the Azure AD applications for the sample. These applicatio
         $tenantId = $creds.Tenant.Id
     }
     $tenant = Get-AzureADTenantDetail
-    $tenantName =  ($tenant.VerifiedDomains | Where { $_._Default -eq $True }).Name
+    $tenantName =  ($tenant.VerifiedDomains | Where-Object { $_._Default -eq $True }).Name
     
     # Removes the applications
     Write-Host "Cleaning-up applications from tenant '$tenantName'"
 
     Write-Host "Removing 'service' (TodoListService-OBO-sample-v2) if needed"
-    $app=Get-AzureADApplication -Filter "DisplayName eq 'TodoListService-OBO-sample-v2'"  
+    Get-AzureADApplication -Filter "DisplayName eq 'TodoListService-OBO-sample-v2'"  | ForEach-Object {Remove-AzureADApplication -ObjectId $_.ObjectId }
+    $apps = Get-AzureADApplication -Filter "DisplayName eq 'TodoListService-OBO-sample-v2'"
+    if ($apps)
+    {
+        Remove-AzureADApplication -ObjectId $apps.ObjectId
+    }
 
-    if ($app)
+    foreach ($app in $apps) 
     {
         Remove-AzureADApplication -ObjectId $app.ObjectId
-        Write-Host "Removed TodoListService-OBO-sample-v2."
+        Write-Host "Removed TodoListService-OBO-sample-v2.."
     }
-        Write-Host "Removing 'client' (TodoListClient-OBO-sample-v2) if needed"
-    $app=Get-AzureADApplication -Filter "DisplayName eq 'TodoListClient-OBO-sample-v2'"  
+    # also remove service principals of this app
+    Get-AzureADServicePrincipal -filter "DisplayName eq 'TodoListService-OBO-sample-v2'" | ForEach-Object {Remove-AzureADServicePrincipal -ObjectId $_.Id -Confirm:$false}
+    
+    Write-Host "Removing 'client' (TodoListClient-OBO-sample-v2) if needed"
+    Get-AzureADApplication -Filter "DisplayName eq 'TodoListClient-OBO-sample-v2'"  | ForEach-Object {Remove-AzureADApplication -ObjectId $_.ObjectId }
+    $apps = Get-AzureADApplication -Filter "DisplayName eq 'TodoListClient-OBO-sample-v2'"
+    if ($apps)
+    {
+        Remove-AzureADApplication -ObjectId $apps.ObjectId
+    }
 
-    if ($app)
+    foreach ($app in $apps) 
     {
         Remove-AzureADApplication -ObjectId $app.ObjectId
-        Write-Host "Removed TodoListClient-OBO-sample-v2."
+        Write-Host "Removed TodoListClient-OBO-sample-v2.."
     }
-        Write-Host "Removing 'spa' (TodoListSPA-OBO-sample-v2) if needed"
-    $app=Get-AzureADApplication -Filter "DisplayName eq 'TodoListSPA-OBO-sample-v2'"  
+    # also remove service principals of this app
+    Get-AzureADServicePrincipal -filter "DisplayName eq 'TodoListClient-OBO-sample-v2'" | ForEach-Object {Remove-AzureADServicePrincipal -ObjectId $_.Id -Confirm:$false}
+    
+    Write-Host "Removing 'spa' (TodoListSPA-OBO-sample-v2) if needed"
+    Get-AzureADApplication -Filter "DisplayName eq 'TodoListSPA-OBO-sample-v2'"  | ForEach-Object {Remove-AzureADApplication -ObjectId $_.ObjectId }
+    $apps = Get-AzureADApplication -Filter "DisplayName eq 'TodoListSPA-OBO-sample-v2'"
+    if ($apps)
+    {
+        Remove-AzureADApplication -ObjectId $apps.ObjectId
+    }
 
-    if ($app)
+    foreach ($app in $apps) 
     {
         Remove-AzureADApplication -ObjectId $app.ObjectId
-        Write-Host "Removed TodoListSPA-OBO-sample-v2."
+        Write-Host "Removed TodoListSPA-OBO-sample-v2.."
     }
-    }
+    # also remove service principals of this app
+    Get-AzureADServicePrincipal -filter "DisplayName eq 'TodoListSPA-OBO-sample-v2'" | ForEach-Object {Remove-AzureADServicePrincipal -ObjectId $_.Id -Confirm:$false}
+    
+}
 
 Cleanup -Credential $Credential -tenantId $TenantId
